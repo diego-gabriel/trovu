6bdd97cd3501e73253412bbc626c015f86c90c01
diego-gabriel <deigo-gabriel@diegogabriel-AO725.(none)>
2014-03-18 22:15:49 -0400

    .

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -3,7 +3,7 @@ class Empresa < ActiveRecord::Base
 	has_many :subCategorias
 	has_many :categorias, :through => subCategoria
 	has_many :sucursals
-	has_many :telefonos, through => sucursal
-	has_many :direccions, through => sucursal
-	has_many :horarios, through => sucursal
+	has_many :telefonos, :through => sucursal
+	has_many :direccions, :through => sucursal
+	has_many :horarios, :through => sucursal
 end
