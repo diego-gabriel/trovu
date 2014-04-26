fef0412e22c8a1b3c5992e949d96622a070fe955
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-04-25 23:53:01 -0400

    :)

--- app/controllers/trovu_controller.rb
+++ app/controllers/trovu_controller.rb
@@ -29,7 +29,8 @@ class TrovuController < ApplicationController
 
   def search
   	word = params[:word]
-    @empresas = Empresa.all
+    word = word.downcase
+    @empresas = Empresa.all.find_all{|e| lDistance(e.nombre, word) <= 3}
   	@lat = -17.38533
   	@lng = -66.15442
   end
@@ -44,6 +45,43 @@ class TrovuController < ApplicationController
   def faq
   end
   def locales
+  end
+
+  private def lDistance(cad, word)
+    require 'matrix'
+    res = 0;
+    dist = []
+
+    for i in 0..(cad.size)
+      dist[i] = []
+      for j in 0..(word.size)
+        dist[i][j] = 0
+      end
+    end
+    
+    for i in 0..(cad.size)
+      dist[i] = []
+      dist[i][0] = i
+    end
+
+    for j in 0..(word.size)
+      dist[0][j] = j
+    end
+
+    for i in 0..(cad.size)
+      for j in 0..(word.size)
+        cost = 1
+        if cad[i] == word[j] then cost = 0
+        end
+        dist[i][j] = [dist[i-1][j] +1,
+                         dist[i][j-1] +1,
+                         dist[i-1][j-1]+cost].min
+      end      
+    end
+
+    res = dist[cad.size][word.size]
+
+    return res
   end
 
 end
